b624b934bea625610832057d058afbaef98d3ed0
Lydell Gaines <dasnic70@gmail.com>
2020-02-19 21:42:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,13 @@
 def oxford_comma(array)
-case array.length 
-end 	when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
+
+oxford_comma(array)
